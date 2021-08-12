8906bf30e062c681836aabb49de31e5e5bb3093b
khafizovas <justhomosapiens121@gmail.com>
2021-08-12 22:01:13 +0300

    03: add recursion solution

--- 03_functions/recursion.js
+++ 03_functions/recursion.js
@@ -23,3 +23,12 @@
   console.log(isEven(-1));
   // → ??
 */
+
+function isEven(number) {
+	if (number === 0) return true;
+	if (number === 1) return false;
+
+	if (number < 0) return isEven(-number);
+
+	return isEven(number - 2);
+}
