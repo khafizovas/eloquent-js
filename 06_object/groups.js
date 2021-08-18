3dbb7af360d97ec122f2409a3e6aa0fa53e958d9
khafizovas <justhomosapiens121@gmail.com>
2021-08-18 21:48:00 +0300

    06/groups: add class interface

--- 06_object/groups.js
+++ 06_object/groups.js
@@ -30,3 +30,25 @@
   console.log(group.has(10));
   // → false
 */
+
+class Group {
+	constructor() {}
+
+	add(value) {}
+
+	delete(value) {}
+
+	has(value) {}
+
+	static from(values) {}
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
